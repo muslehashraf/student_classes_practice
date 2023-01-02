16d84b83c38e700c9989ad80496685fd082bb2fb
Ashraf Musleh <muslehashraf@gmail.com>
2023-01-01 22:04:56 -0600

    ..:

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -22,5 +26,64 @@ public class Test {
         Print how many students are MathStudent with message -> "Math students = {numberOfMathStudents}"
         Print how many students are ScienceStudent with message -> "Science students = {numberOfScienceStudents}"
          */
+
+        Scanner scanner = new Scanner(System.in);
+        int numOfStudents = 0;
+        int numOfMathStudents = 0;
+        int numOfScienceStudents = 0;
+        List<Student> students = new ArrayList<>();
+
+        while(students.size() < 3){
+            System.out.println(UserQuestions.askToJoin);
+            String answer = scanner.nextLine();
+            if (answer.equalsIgnoreCase("N"))continue;
+
+            System.out.println(UserQuestions.askFirstName);
+            String firstName = scanner.nextLine();
+            System.out.println(UserQuestions.askLastName);
+            String lastName = scanner.nextLine();
+            System.out.println(UserQuestions.askAge);
+            int age = scanner.nextInt();
+            scanner.nextLine();
+
+            try {
+                Permission.checkAge(age);
+            } catch (RuntimeException c) {
+                c.printStackTrace();
+                scanner.nextLine();
+                continue;
+            }
+
+            System.out.println(UserQuestions.askGender);
+            String gender = scanner.nextLine();
+            System.out.println(UserQuestions.askClassName);
+            String className = scanner.nextLine();
+
+            try {
+                Permission.checkClassName(className);
+            } catch (RuntimeException c) {
+                c.printStackTrace();
+                continue;
+            }
+
+            if (className.equalsIgnoreCase("math")) {
+                MathStudent mathStudent = new MathStudent(firstName, lastName, age, gender, className);
+                students.add(mathStudent);
+                numOfMathStudents++;
+                System.out.println("Congratulations! You are registered for " + className + " class.");
+            } else {
+                ScienceStudent scienceStudent = new ScienceStudent(firstName, lastName, age, gender, className);
+                students.add(scienceStudent);
+                numOfScienceStudents++;
+                System.out.println("Congratulations! You are registered for " + className + " class.");
+            }
+        }
+
+        for (Student student : students) {
+            System.out.println(student);
+        }
+
+        System.out.println("Math Students = " + numOfMathStudents);
+        System.out.println("Science Students = " + numOfScienceStudents);
     }
 }
